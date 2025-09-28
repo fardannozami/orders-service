f1519be39cb15d2f593a9988eef4556789c80958
nozami <fardannozami@gmail.com>
2025-09-28 16:59:56 +0700

    adding redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersController } from './orders/orders.controller';
+import { Client, ClientsModule, Transport } from '@nestjs/microservices';
+import { MICROSERVICE_CLIENT } from './constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: MICROSERVICE_CLIENT.PRODUCT_REDIS_CLIENT,
+        transport: Transport.REDIS,
+        options:{
+          host: '127.0.0.1',
+          port: 6379
+        }
+      }
+    ])
+  ],
   controllers: [AppController, OrdersController],
   providers: [AppService],
 })
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,11 +1,14 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { Controller, Inject } from '@nestjs/common';
+import { ClientProxy, MessagePattern } from '@nestjs/microservices';
+import { MICROSERVICE_CLIENT } from 'src/constants';
 
 @Controller('orders')
 export class OrdersController {
+    constructor(@Inject(MICROSERVICE_CLIENT.PRODUCT_REDIS_CLIENT) private productRedisClient: ClientProxy) {}
     @MessagePattern('create_order')
     createOrder(data: any) {
         console.log({message: 'Order received on orders-service', data});
-        return { message: 'Order created', data };
+        this.productRedisClient.emit('order.created', data);
+        return this.productRedisClient.send('get_product', data.productId);
     }
 }
